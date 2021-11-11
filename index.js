cf0fc1e2075124bc95e6f4ec7270d85bebe53adf
Vinh <vinhnk2300@gmail.com>
2021-11-11 11:34:25 +0700

    Thêm Layout
    Thêm:
     - Giao diện cho page Newfeed
     - Chỉnh sửa PORT 8080
     - Jquery, css
     -  Sửa đổi bên trong package.json
     - Dùng lệnh nodemon thay cho npm run
     - Thêm file packagelock

---
+++ index.js
@@ -0,0 +1,48 @@
+// Khai báo package
+const dotenv = require('dotenv').config({ path: './.env' });
+const rateLimit = require('express-rate-limit');
+const exphbs = require('express-handlebars');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sử dụng tính năng rate limit trong express để phòng chống tấn công DDOS.
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 60 * 1000, // 15 minutes
+  max: 100, // Giới hạn mỗi IP chỉ cho 100 request ( Đơn vị windowMs )
+});
+
+// Cors chia sẻ tài nguyên chéo liên quan đến bảo mật ngăn chặn truy cập tài nguyên của các domain khác
+app.use(cors());
+// app.use(limiter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '50mb' })); // Limit kiểm soát số lượng request body
+
+// Khai báo folder public để thêm styling và scripts
+app.use(express.static('public'));
+
+// Khai báo view engine
+app.engine(
+  'hbs',
+  exphbs({
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts/',
+    partialsDir: __dirname + '/views/partials/',
+    extname: '.hbs',
+    helpers: {},
+  })
+);
+app.set('view engine', 'hbs');
+
+// Đặt đường dẫn là views vì default của view engine chỉ nhận folder từ root
+app.set('views', path.join(__dirname, 'views'));
+
+// Khai báo routers
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(PORT, () => console.log(`Server is online at ${PORT}`));
