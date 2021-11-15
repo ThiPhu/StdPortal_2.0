c2a1cb053f0d0ad390a6d86bc7f9d6c3f7a69228
Vinh <vinhnk2300@gmail.com>
2021-11-16 01:39:11 +0700

    Update
    Cập nhật:
     - Thay đổi khai bao MongoDB
     - Thêm folder admin và user trong views
     - Thêm layout management cho admin
     - Chưa chỉnh css

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,25 @@
-const apiRouter = require("./api.routes")
+const apiRouter = require('./api.routes');
 
-const route = (app)=>{
+const route = app => {
+  // Các route api
+  app.use('/api', apiRouter);
 
-    // Các route api
-    app.use("/api", apiRouter)
+  // Các route không yêu cầu phiên đăng nhập của user
+  app.get('/login', (req, res) => {
+    res.render('login');
+  });
 
-    // Các route không yêu cầu phiên đăng nhập của user
-    app.get("/login", (req,res)=>{
-        res.render("login")
-    })
+  app.get('/management', (req, res) => {
+    res.render('admin/management');
+  });
 
-    
+  // Các route yêu cầu phiên đăng nhập của user
+  app.get('/', (req, res) => {
+    res.render('home');
+  });
 
-    // Các route yêu cầu phiên đăng nhập của user
-    app.get("/", (req,res)=>{
-        res.render("home")
-    })
+  // 404
+  app.use((req, res) => res.redirect('/404'));
+};
 
-
-    // 404
-    app.use((req,res)=> res.redirect("/404"))
-}
-
-module.exports = route
+module.exports = route;
