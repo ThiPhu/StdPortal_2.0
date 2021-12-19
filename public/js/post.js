a85b3ea0db605fef1461a44716b92f0b08f43dbb
Vinh <vinhnk2300@gmail.com>
2021-12-19 10:09:01 +0700

    Update
    Thêm phần ajax dể load comment ( Chưa hoàn thiện )

--- public/js/post.js
+++ public/js/post.js
@@ -1,12 +1,52 @@
 $(document).ready(function () {
   // Bấm vào comment btn sẽ hiện các comment dưới bài viết
+  //var templateMissions = Handlebars.compile($('#comment_data_template').html());
+  //   function printData(datas) {
+  //     let posts = datas.posts;
+  //     console.log(posts.length);
+  //     for (var i = 0; i < posts.length; i++) {
+  //       var data = datas.posts[i];
+  //       console.log(data);
+  //       var dataStamp = {
+  //         comments: data.comments,
+  //       };
+  //       console.log(dataStamp);
+  //       var template = templateMissions(dataStamp);
+  //       $('.comment_container').append(template);
+  //     }
+  //   }
   $('.comments_expand').one('click', e => {
     e.preventDefault();
     let postId = $(e.target).closest('.post_id').data('postid');
-    $('#comment_loading_' + postId).removeClass('d-none');
-    setTimeout(() => {
-      $('#comment_' + postId).removeClass('d-none');
-      $('#comment_loading_' + postId).addClass('d-none');
-    }, 1000);
+    $.ajax({
+      url: '/api/post',
+      method: 'GET',
+      async: true,
+      beforeSend: function () {
+        $('#comment_loading_' + postId).removeClass('d-none');
+      },
+      success: function (data) {
+        let posts = data.posts;
+        let commentsTemplate = Handlebars.compile(
+          $('#comment_data_template').html()
+        );
+        for (var i = 0; i < posts.length; i++) {
+          let datas = data.posts[i];
+          if (datas._id === postId) {
+            let dataStamp = {
+              comments: datas.comments,
+            };
+            console.log(commentsTemplate(null));
+            let template = commentsTemplate(dataStamp);
+            $('.comment_container').append(template);
+          }
+        }
+        $('#comment_' + postId).removeClass('d-none');
+        $('#comment_loading_' + postId).addClass('d-none');
+      },
+      error: function () {
+        alert('error');
+      },
+    });
   });
 });
