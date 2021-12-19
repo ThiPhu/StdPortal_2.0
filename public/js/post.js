ed787aca5f3b39a3ac273f3e16245222177c648c
Vinh <vinhnk2300@gmail.com>
2021-12-19 20:45:45 +0700

    Update
    Success load comment from ajax

--- public/js/post.js
+++ public/js/post.js
@@ -1,45 +1,28 @@
 $(document).ready(function () {
   // Bấm vào comment btn sẽ hiện các comment dưới bài viết
-  //var templateMissions = Handlebars.compile($('#comment_data_template').html());
-  //   function printData(datas) {
-  //     let posts = datas.posts;
-  //     console.log(posts.length);
-  //     for (var i = 0; i < posts.length; i++) {
-  //       var data = datas.posts[i];
-  //       console.log(data);
-  //       var dataStamp = {
-  //         comments: data.comments,
-  //       };
-  //       console.log(dataStamp);
-  //       var template = templateMissions(dataStamp);
-  //       $('.comment_container').append(template);
-  //     }
-  //   }
   $('.comments_expand').one('click', e => {
     e.preventDefault();
     let postId = $(e.target).closest('.post_id').data('postid');
     $.ajax({
-      url: '/api/post',
+      url: '/api/post/' + postId,
       method: 'GET',
       async: true,
       beforeSend: function () {
         $('#comment_loading_' + postId).removeClass('d-none');
       },
       success: function (data) {
-        let posts = data.posts;
-        let commentsTemplate = Handlebars.compile(
-          $('#comment_data_template').html()
-        );
-        for (var i = 0; i < posts.length; i++) {
-          let datas = data.posts[i];
-          if (datas._id === postId) {
-            let dataStamp = {
-              comments: datas.comments,
-            };
-            console.log(commentsTemplate(null));
-            let template = commentsTemplate(dataStamp);
-            $('.comment_container').append(template);
-          }
+        const comments = data.post.comments.length;
+        for (var i = 0; i < comments; i++) {
+          let commentsTemplate = Handlebars.compile(
+            $('.comment_data_template_' + postId).html()
+          );
+
+          let dataStamp = {
+            comments: data.post.comments[i],
+          };
+          var template = commentsTemplate(dataStamp);
+          console.log(dataStamp.comments);
+          $('.comment_container_' + postId).html(template);
         }
         $('#comment_' + postId).removeClass('d-none');
         $('#comment_loading_' + postId).addClass('d-none');
