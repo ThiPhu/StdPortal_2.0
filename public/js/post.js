f6b888e6edd7c86f866b999639857ef67f5936f6
zedpro1230 <73679480+zedpro1230@users.noreply.github.com>
2021-12-26 18:09:30 +0700

    add create post UI

--- public/js/post.js
+++ public/js/post.js
@@ -50,28 +50,28 @@ $(document).ready(function () {
 });
 //review img before post
 function img_upload() {
-  document.getElementById('file').click();
-}
-input_img = document.getElementById('input_img');
-var review = document.getElementById('review');
-
-var loadFile = function (event) {
+  document.getElementById("file").click();
+};
+input_img=document.getElementById('input_img')
+var re_img=document.getElementById('review_img')
+var show_img=document.getElementById('review')
+console.log(input_img)
+var loadFile = function(event) {
   var reader = new FileReader();
-  reader.onload = function () {
+  reader.onload = function(){
     var output = document.getElementById('review_img');
     output.src = reader.result;
   };
   reader.readAsDataURL(event.target.files[0]);
-  review.style.display = 'block';
-  re_img.style.display = 'block';
-  input_img.style.display = 'none';
+  re_img.style.display='block'
+  show_img.style.display='block'
+  input_img.style.display='none'
 };
 //remove img
-var re_img = document.getElementById('review_img');
-function remove() {
+function remove(){
   re_img.removeAttribute('src');
-  document.getElementById('file').value = null;
-  re_img.style.display = 'none';
-  review.style.display = 'none';
-  input_img.style.display = 'block';
+  document.getElementById("file").value=null
+  re_img.style.display='none'
+  show_img.style.display='none'
+  input_img.style.display='block'
 }
