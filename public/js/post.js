1f97acc1031fa6cb92a1e8e94b3ce2aca29e4d81
Vinh <vinhnk2300@gmail.com>
2021-12-28 02:45:26 +0700

    Update
    Cập nhật giao diện post
    Có thể xoá - đăng post
    Comment chưa hoàn thiện

--- public/js/post.js
+++ public/js/post.js
@@ -21,7 +21,9 @@ $(document).ready(function () {
             comments: data.post.comments[i],
           };
           var template = commentsTemplate(dataStamp);
-          $('.comment_container_' + postId).html(template);
+          $('.comment_container_' + postId).html(
+            $('.post_comments').html() + template
+          );
         }
         $('#comment_' + postId).removeClass('d-none');
         $('#comment_loading_' + postId).addClass('d-none');
@@ -32,46 +34,104 @@ $(document).ready(function () {
     });
   });
 
-  //auto resize text area
+  // Tạo bài viết
+  $('.create_post-submitBtn').on('click', e => {
+    e.preventDefault();
+    const caption = $('.create_post-input').val();
+    const image = $('.create_post-image')[0].files[0];
+    const formData = new FormData();
+    formData.append('caption', caption);
+    formData.append('image', image);
+    fetch('/api/post', {
+      method: 'POST',
+      async: true,
+      body: formData,
+    })
+      .then(res => res.json())
+      .then(({ ok, msg, at }) => {
+        // If auth success, redirect to home
+        return (window.location.href = '/');
+      });
+  });
 
-  const tx = $('.comment_box');
-  for (let i = 0; i < tx.length; i++) {
-    tx[i].setAttribute(
-      'style',
-      'height:' + tx[i].scrollHeight + 'px;overflow-y:hidden;'
-    );
-    tx[i].addEventListener('input', OnInput, false);
-  }
-  function OnInput() {
-    this.style.height = 'auto';
-    this.style.height = this.scrollHeight + 'px';
-  }
-  
+  // Xoá bài viết
+  $('.post_delete_Btn').on('click', e => {
+    e.preventDefault();
+    let postId = $(e.target).closest('.post_id').data('postid');
+    Swal.fire({
+      title: 'Xoá bài viết này ?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#0e6286',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Xác nhận',
+      cancelButtonText: 'Huỷ',
+      keydownListenerCapture: true,
+      allowOutsideClick: false,
+    }).then(result => {
+      if (result.isConfirmed) {
+        fetch('/api/post/' + postId, {
+          method: 'DELETE',
+        })
+          .then(res => res.json())
+          .then(({ ok, msg, at }) => {
+            // If auth success, redirect to home
+            return (window.location.href = '/');
+          });
+        Swal.fire({
+          title: 'Xoá bài viết thành công.',
+          confirmButtonText: 'Xác nhận!',
+          confirmButtonColor: '#0e6286',
+        });
+      }
+    });
+  });
+
+  // Tạo bình luận
+  $('.comment_box').keypress(e => {
+    let postId = $(e.target).closest('.post_id').data('postid');
+    if (e.which == 13 && !e.shiftKey) {
+      e.preventDefault();
+      const comment = $(e.target).val();
+      fetch('/api/comment/', {
+        method: 'POST',
+        body: new URLSearchParams({
+          content: comment,
+          postId: postId,
+        }),
+      })
+        .then(res => res.json())
+        .then(({ ok, msg, at }) => {
+          // If auth success, redirect to home
+          return (window.location.href = '/');
+        });
+    }
+  });
 });
+
 //review img before post
 function img_upload() {
-  document.getElementById("file").click();
-};
-input_img=document.getElementById('input_img')
-var re_img=document.getElementById('review_img')
-var show_img=document.getElementById('review')
-console.log(input_img)
-var loadFile = function(event) {
+  document.getElementById('file').click();
+}
+input_img = document.getElementById('input_img');
+var re_img = document.getElementById('review_img');
+var show_img = document.getElementById('review');
+var loadFile = function (event) {
   var reader = new FileReader();
-  reader.onload = function(){
+  reader.onload = function () {
     var output = document.getElementById('review_img');
     output.src = reader.result;
   };
   reader.readAsDataURL(event.target.files[0]);
-  re_img.style.display='block'
-  show_img.style.display='block'
-  input_img.style.display='none'
+  re_img.style.display = 'block';
+  show_img.style.display = 'block';
+  input_img.style.display = 'none';
 };
 //remove img
-function remove(){
+function remove() {
   re_img.removeAttribute('src');
-  document.getElementById("file").value=null
-  re_img.style.display='none'
-  show_img.style.display='none'
-  input_img.style.display='block'
+  document.getElementById('file').value = null;
+  re_img.style.display = 'none';
+  show_img.style.display = 'none';
+  input_img.style.display = 'block';
 }
